2bb72e106f1cf9d0b2df152f1940bca78e2288c7
Chris <clavoie@website.com>
2014-03-22 15:58:36 -0400

    added OAuth token for google. We can now sign into the app with our google accounts

--- config/env/development.js
+++ config/env/development.js
@@ -20,8 +20,8 @@ module.exports = {
         callbackURL: 'http://localhost:3000/auth/github/callback'
     },
     google: {
-        clientID: 'APP_ID',
-        clientSecret: 'APP_SECRET',
+        clientID: '202369370759-9r7g79uttn2qr2ltl1laaf405chp1uu6.apps.googleusercontent.com',
+        clientSecret: 'uFmNYhKk5aa82tIisVGmr0UE',
         callbackURL: 'http://localhost:3000/auth/google/callback'
     },
     linkedin: {
